8d9fa99b615678bf2926653696152e95e39189fc
Percy Hu <powerofpercy@outlook.com>
2017-01-19 00:21:16 +1300

    Added Player Movement and Shooting Prototype.

---
+++ Prototypes/Player Movement+Shooting/Assets/Scripts/HitWall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitWall : MonoBehaviour {
+
+	
+	void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Wall") {
+            Destroy(gameObject);
+        }
+   }
+}
