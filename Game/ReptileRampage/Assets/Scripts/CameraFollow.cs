b5b910cdb05aece7b27deb724274c6a98eb363c9
Joe-PC\Joe <joe.gasparich@hotmail.com>
2017-01-24 03:08:21 +1300

    Fixed camera follow (Wasn't following the player off screen)

--- Game/ReptileRampage/Assets/Scripts/CameraFollow.cs
+++ Game/ReptileRampage/Assets/Scripts/CameraFollow.cs
@@ -4,16 +4,13 @@ public class CameraFollow : MonoBehaviour {
 	
 	public GameObject player;
 
+	public float followDistance;
     public static Vector3 cameraPosition;
-
-    void Start () 
-    {
     
-    }
-    
-    void Update () 
-    {   
-        transform.position = new Vector3((Camera.main.ScreenToWorldPoint(Input.mousePosition).x+player.transform.position.x)/4, (Camera.main.ScreenToWorldPoint(Input.mousePosition).y+player.transform.position.y)/4, -10);
+    void Update () {
+		float x = player.transform.position.x + ((Camera.main.ScreenToWorldPoint (Input.mousePosition).x - player.transform.position.x) / followDistance);
+		float y = player.transform.position.y + ((Camera.main.ScreenToWorldPoint (Input.mousePosition).y - player.transform.position.y) / followDistance);
+		transform.position = new Vector3(x, y, -10);
         cameraPosition = transform.position;
     }
 }
