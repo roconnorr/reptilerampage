4e57e5b953e741f1a113527ac59704a867c41287
Joseph Gasparich <gasjo158@student.otago.ac.nz>
2017-01-23 14:37:07 +1300

    Bullet code refactored, bullets are now destroyed when they go off screen, explosions are destroyed when their animation ends

---
+++ Game/ReptileRampage/Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+	
+	public int moveSpeed;
+	
+	void Update () {
+		transform.Translate (Vector3.right * Time.deltaTime * moveSpeed);
+		// Check if the game object is visible, if not, destroy self   
+		if(!Utility.isVisible(GetComponent<Renderer>(), Camera.main)) {
+			Destroy(gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.tag == "Wall"){
+			Explode();
+			Destroy(gameObject);
+		}
+		if(other.gameObject.tag == "Player"){
+			Physics2D.IgnoreCollision(other.collider, gameObject.GetComponent<Collider2D>());
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if(other.gameObject.tag == "Enemy"){
+			Explode();
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+
+	void Explode(){
+		GameObject explosion = (GameObject)Instantiate(explosionPrefab);
+		explosion.transform.position = transform.position;
+	}
+}
---
+++ Game/ReptileRampage/Assets/Scripts/Utility.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Utility : MonoBehaviour {
+
+	public static bool isVisible(Renderer renderer, Camera camera) {
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+}
