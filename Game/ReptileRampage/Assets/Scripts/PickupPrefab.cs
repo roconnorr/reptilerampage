93ffa6c13f18e0b140291b902aa6a1dc0e62ab28
Rory-mbp <roconnor@Rorys-MBP.fritz.box>
2017-01-30 18:22:35 +1300

    Assets for weapon switching

---
+++ Game/ReptileRampage/Assets/Scripts/PickupPrefab.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupPrefab : MonoBehaviour {
+
+	public Player.WeaponType type;
+
+	void Start(){
+		//set sprite
+	}
+
+	void Update(){
+		//set sprite
+	}
+
+	public void ChangeType(Player.WeaponType newType){
+		type = newType;
+	}
+}
