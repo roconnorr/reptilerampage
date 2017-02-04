d5fb0b95501743d814f0a70b633aa5fdba613571
rory-home <asdf@asdf.com>
2017-02-04 23:35:29 +1300

    Added boss health bars to scenes 1 and 2

--- Game/ReptileRampage/Assets/Scripts/Boss Fights/TrikeFight.cs
+++ Game/ReptileRampage/Assets/Scripts/Boss Fights/TrikeFight.cs
@@ -6,6 +6,8 @@ public class TrikeFight : MonoBehaviour {
 	private GameObject trikeInstance;
 	public GameObject player;
 	public GameObject DialogBox;
+	private GameObject canvas;
+	private HUDManager hudManager;
 
 	public void StartFight(){
 		DialogBox.GetComponentInParent<TextBoxManager>().dialogActive = true;
@@ -14,6 +16,11 @@ public class TrikeFight : MonoBehaviour {
 	public void SpawnTrike(){
 		trikeInstance = Instantiate(trike, new Vector3(12, 47, -1), new Quaternion(0,0,0,0));
 		trikeInstance.GetComponent<Trike>().target = player.transform;
+		canvas = GameObject.Find("Canvas");
+		hudManager = canvas.GetComponent<HUDManager>();
+		hudManager.levelBoss = trikeInstance;
+		hudManager.inBossFight = true;
+		hudManager.SetBossHealthActive(true);
 		this.gameObject.SetActive(false);
 	}
 
