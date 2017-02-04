5c46af7a58ab267fcec98264ba56cd9d677a2ed4
Joe-PC\Joe <joe.gasparich@hotmail.com>
2017-02-04 20:14:38 +1300

    Diplo fires laser and moves

---
+++ Game/ReptileRampage/Assets/Scripts/Gavin.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gavin : MonoBehaviour {
+
+	private enum State {Idle, Laser, Shooting, Spawning};
+
+	private State state;
+
+	private SpriteRenderer sr;
+	private Transform laser;
+	private Transform firePoint;
+
+	public Transform target;
+	public Transform bulletPrefab;
+
+	public float speed;
+	public int laserDamage;
+
+	[HideInInspector]
+	public int dir = 1;
+	private bool isLaser = false;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+		laser = transform.FindChild ("Laser");
+		firePoint = transform.FindChild ("FirePoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (0, speed * 0.02f * dir));
+		if (state == State.Idle && Random.Range (0, 200) == 1) {
+			state = State.Laser;
+		}
+		if (state == State.Laser && !isLaser) {
+			isLaser = true;
+			sr.color = Color.blue;
+			Invoke ("FireLaser", 1f);
+		}
+	}
+
+	void FireLaser() {
+		laser.gameObject.SetActive (true);
+		Invoke ("CancelLaser", 3f);
+		sr.color = Color.white;
+	}
+
+	void CancelLaser() {
+		laser.gameObject.SetActive (false);
+		isLaser = false;
+		state = State.Idle;
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "GavinCollider") {
+			dir *= -1;
+		}
+	}
+}
---
+++ Game/ReptileRampage/Assets/Scripts/Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+	private Vector3 position;
+
+	void Start() {
+		position = new Vector3 (transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+	}
+
+	void Update() {
+		transform.localPosition = position;
+		float shakeY = Random.Range(-0.05f, 0.05f);
+		Vector3 pp = transform.position;
+		pp.y += shakeY;
+		transform.position = pp;
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.tag == "Player"){
+			float angle =Mathf.Atan2(other.transform.position.y-transform.position.y, transform.position.x-transform.position.x)*180 / Mathf.PI;
+			angle -= 90;
+			other.gameObject.GetComponent<Player>().TakeDamage (GetComponentInParent<Gavin>().laserDamage, Quaternion.Euler(0, 0, angle), 500);
+		}
+	}
+}
