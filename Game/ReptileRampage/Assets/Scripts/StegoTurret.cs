165a839ccd38a008b497ff6c12415ce2056c1676
Percy Hu <powerofpercy@outlook.com>
2017-02-06 21:55:57 +1300

    Fixed exception where the Stego Turrent doesn’t get destroyed after player dies.

--- Game/ReptileRampage/Assets/Scripts/StegoTurret.cs
+++ Game/ReptileRampage/Assets/Scripts/StegoTurret.cs
@@ -31,6 +31,10 @@ public class StegoTurret : MonoBehaviour {
 	}
 
 	void Update () {
+		if(target == null){
+			Destroy(gameObject);
+			return;
+		}
 		targetInShootRange = Vector3.Distance(gameObject.transform.position, target.transform.position) < shootRange;
 
 		if(targetInShootRange && GetComponentInParent<Stegosaurus>().isChasing){
