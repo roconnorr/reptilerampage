7b18f64581bd5f04a4a256883c620879fb8e780f
Percy Hu <powerofpercy@outlook.com>
2017-02-08 01:09:58 +1300

    Added gate (suppose to be a broken bridge) to level 2, reseted player positions and organised stuff.

--- Game/ReptileRampage/Assets/Scripts/Gate.cs
+++ Game/ReptileRampage/Assets/Scripts/Gate.cs
@@ -5,8 +5,9 @@ public class Gate : MonoBehaviour {
 	public Transform player;
 	public GameObject canvas;
 	private HUDManager hudManager;
-
 	private Animator animator;
+	public bool isLevel1;
+	public bool isLevel2;
 
 	void Start () {
 		hudManager = canvas.GetComponent<HUDManager>();
@@ -16,14 +17,21 @@ public class Gate : MonoBehaviour {
 	void Update () {
 		float dist = Vector3.Distance(player.position, transform.position);
 
-		if(dist > 2f && player.position.x < transform.position.x && hudManager.inBossFight){
-			animator.Play("GateClosing");
-			if(this.animator.GetCurrentAnimatorStateInfo(0).IsName("GateClosed")){
-				animator.Play("GateClosed");
+		if(isLevel1){
+			if(dist > 2f && player.position.x < transform.position.x && hudManager.inBossFight){
+				animator.Play("GateClosing");
+				if(this.animator.GetCurrentAnimatorStateInfo(0).IsName("GateClosed")){
+					animator.Play("GateClosed");
+				}
+				gameObject.GetComponent<Collider2D>().enabled = true;
+			}else if(!hudManager.inBossFight){
+				animator.Play("GateIdle");
 			}
-			gameObject.GetComponent<Collider2D>().enabled = true;
-		}else if(!hudManager.inBossFight){
-			animator.Play("GateIdle");
+		} else if(isLevel2){
+			//Debug.Log(dist);
+			if(dist > 6f && player.position.y > transform.position.y && hudManager.inBossFight){
+				gameObject.GetComponent<Collider2D>().enabled = true;
+			}
 		}
 	}
 }
