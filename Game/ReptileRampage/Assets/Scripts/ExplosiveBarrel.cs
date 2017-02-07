380644fb38cd5fef0c843784d37a5b12928a7af1
Percy Hu <powerofpercy@outlook.com>
2017-02-07 17:08:43 +1300

    Barrels now only explode when in player’s view.

--- Game/ReptileRampage/Assets/Scripts/ExplosiveBarrel.cs
+++ Game/ReptileRampage/Assets/Scripts/ExplosiveBarrel.cs
@@ -7,13 +7,17 @@ public class ExplosiveBarrel : MonoBehaviour {
 	public float radius = 3.0F;
     public float power = 300.0F;
 	public int explodeDamage = 30;
-    
+    private Camera mainCamera;
     void Start() {
-		//soundSource = gameObject.GetComponent<AudioSource>();
+		mainCamera = Camera.main;
     }
 
 	public void Destroy() {
-		GameMaster.CreateExplosion(animationPrefab, explosionScriptPrefab ,transform.position, explodeDamage, power, radius, false);
-		Destroy (gameObject);
+		Vector3 screenPoint = mainCamera.WorldToViewportPoint(transform.position);
+ 		bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+		if(onScreen){
+			GameMaster.CreateExplosion(animationPrefab, explosionScriptPrefab ,transform.position, explodeDamage, power, radius, false);
+			Destroy (gameObject);
+		}
 	}
 }
