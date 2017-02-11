4760f602d1569892bb7437545e7fdd5898d4d7e5
rory-home <asdf@asdf.com>
2017-02-12 12:15:04 +1300

    Added arena to title screen

--- Game/ReptileRampage/Assets/Scripts/UI/TitleScreenManager.cs
+++ Game/ReptileRampage/Assets/Scripts/UI/TitleScreenManager.cs
@@ -4,21 +4,29 @@ using UnityEngine.SceneManagement;
 public class TitleScreenManager : MonoBehaviour {
 
 	public GameObject ButtonStart;
+	public GameObject ButtonArena;
 	public GameObject ButtonQuit;
+	public string ArenaPassword;
 
 	void Start(){
 		ButtonStart.SetActive(false);
 		ButtonQuit.SetActive(false);
+		ButtonArena.SetActive(false);
 		Cursor.visible = true;
 	}
 
 	public void EnableButtons(){
 		ButtonStart.SetActive(true);
 		ButtonQuit.SetActive(true);
+		ButtonArena.SetActive(true);
 	}
    
    public void StartGame () {
       SceneManager.LoadScene("Level1");   
+   }
+
+   public void StartArena(){
+	   SceneManager.LoadScene("Arena");
    }
    
    public void QuitGame() {
