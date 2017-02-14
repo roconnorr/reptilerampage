d18666ca24b372bd62ba7ea3da6146137d3e926f
Percy Hu <powerofpercy@outlook.com>
2017-02-14 13:58:35 +1300

    “optimisation”

--- Game/ReptileRampage/Assets/Scripts/DisableDialog.cs
+++ Game/ReptileRampage/Assets/Scripts/DisableDialog.cs
@@ -2,15 +2,9 @@
 
 public class DisableDialog : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 		if(WayPoints.respawned){
 			gameObject.SetActive(false);
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
