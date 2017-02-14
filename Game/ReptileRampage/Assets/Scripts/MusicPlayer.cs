b75d6c7c9e2e1bb9bb53c8962a5752e2daa5b462
Percy Hu <powerofpercy@outlook.com>
2017-02-15 00:11:13 +1300

    Added music player to title screen.

--- Game/ReptileRampage/Assets/Scripts/MusicPlayer.cs
+++ Game/ReptileRampage/Assets/Scripts/MusicPlayer.cs
@@ -9,22 +9,29 @@ public class MusicPlayer : MonoBehaviour {
 	public GameObject canvas;
 	private bool levelMusicPlayed;
 	private bool bossMusicPlayed;
+	public bool titleScreen;
 	void Start () {
 		musicPlayer = GetComponent<AudioSource>();
 		hudManager = canvas.GetComponent<HUDManager>();
+		if(titleScreen){
+			musicPlayer.clip = levelMusic;
+			musicPlayer.Play();
+		}
 	}
 	
 	void Update () {
-		if(hudManager.inBossFight && !bossMusicPlayed){
-			musicPlayer.clip = bossMusic;
-			musicPlayer.Play();
-			bossMusicPlayed = true;
-			levelMusicPlayed = false;
-		}else if(!hudManager.inBossFight && !levelMusicPlayed){
-			musicPlayer.clip = levelMusic;
-			musicPlayer.Play();
-			levelMusicPlayed = true;
-			bossMusicPlayed = false;
+		if(!titleScreen){
+			if(hudManager.inBossFight && !bossMusicPlayed){
+				musicPlayer.clip = bossMusic;
+				musicPlayer.Play();
+				bossMusicPlayed = true;
+				levelMusicPlayed = false;
+			}else if(!hudManager.inBossFight && !levelMusicPlayed){
+				musicPlayer.clip = levelMusic;
+				musicPlayer.Play();
+				levelMusicPlayed = true;
+				bossMusicPlayed = false;
+			}
 		}
 	}
 }
