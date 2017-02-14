840d895f4fc00498b8d09a90fcf3d94311c6826d
Joe-PC\Joe <joe.gasparich@hotmail.com>
2017-02-14 23:41:30 +1300

    Added input to take you to a certain level for presentation

---
+++ Game/ReptileRampage/Assets/Scripts/Presentation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Presentation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Level1")) {
+			SceneManager.LoadScene("Level1");
+		}
+		if (Input.GetButtonDown ("Level2")) {
+			SceneManager.LoadScene("Level2");
+		}
+		if (Input.GetButtonDown ("Level3")) {
+			SceneManager.LoadScene("Level3");
+		}
+		if (Input.GetButtonDown ("Level4")) {
+			SceneManager.LoadScene("FinalBoss");
+		}
+	}
+}
