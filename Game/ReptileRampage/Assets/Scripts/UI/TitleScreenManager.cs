ea633fdf780b031d3a332394965b75483757e619
rory-home <asdf@asdf.com>
2017-02-16 12:28:24 +1300

    Added credits to title screen

--- Game/ReptileRampage/Assets/Scripts/UI/TitleScreenManager.cs
+++ Game/ReptileRampage/Assets/Scripts/UI/TitleScreenManager.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScreenManager : MonoBehaviour {
 
 	public GameObject ButtonStart;
 	public GameObject ButtonArena;
 	public GameObject ButtonQuit;
-	public string ArenaPassword;
+	public GameObject ButtonCredits;
+	public GameObject ButtonCreditsQuit;
+	public Image CreditsImage;
 
 	void Start(){
 		ButtonStart.SetActive(false);
 		ButtonQuit.SetActive(false);
 		ButtonArena.SetActive(false);
+		ButtonCredits.SetActive(false);
+		ButtonCreditsQuit.SetActive(false);
+		CreditsImage.enabled = false;
 		Cursor.visible = true;
 	}
 
@@ -19,6 +25,7 @@ public class TitleScreenManager : MonoBehaviour {
 		ButtonStart.SetActive(true);
 		ButtonQuit.SetActive(true);
 		ButtonArena.SetActive(true);
+		ButtonCredits.SetActive(true);
 	}
    
    public void StartGame () {
@@ -27,6 +34,15 @@ public class TitleScreenManager : MonoBehaviour {
 
    public void StartArena(){
 	   SceneManager.LoadScene("Arena");
+   }
+
+   public void DisplayCredits(){
+	   CreditsImage.enabled = true;
+	   ButtonCreditsQuit.SetActive(true);
+   }
+   public void HideCredits(){
+	   CreditsImage.enabled = false;
+	   ButtonCreditsQuit.SetActive(false);
    }
    
    public void QuitGame() {
