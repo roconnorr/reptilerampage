91faea0573fe1aa30f813cb03ba811b349a02195
rory-home <asdf@asdf.com>
2017-02-16 16:30:51 +1300

    Boss stompings

--- Game/ReptileRampage/Assets/Scripts/CameraShake.cs
+++ Game/ReptileRampage/Assets/Scripts/CameraShake.cs
@@ -5,10 +5,18 @@ public class CameraShake : MonoBehaviour {
 
 	private Camera mainCamera;
 
+	public AudioClip stomp;
+
 	void Start(){
 		mainCamera = Camera.main;
 	}
 	public void StartShaking(float shakeAmt) {
+		if(GetComponent<Trike>() && stomp != null) {
+			PlayHitSound(stomp, this.transform.position);
+		}
+		if(GetComponent<TRex>() && stomp != null) {
+			PlayHitSound(stomp, this.transform.position);
+		}
 		this.shakeAmt = shakeAmt;
 		InvokeRepeating("ShakeCamera", 0, .02f);
 		Invoke("StopShaking", 0.16f);
@@ -34,5 +42,16 @@ public class CameraShake : MonoBehaviour {
 		} else if(!WayPoints.heliMoving){
 			mainCamera.transform.position = new Vector3 (12, 50, -15);
 		}
+	}
+	public static void PlayHitSound(AudioClip clip, Vector3 pos){
+		GameObject temp = new GameObject("TempAudio");
+		temp.transform.position = pos;
+		AudioSource tempSource = temp.AddComponent<AudioSource>();
+		tempSource.clip = clip;
+		tempSource.volume = 0.15f;
+		if(!tempSource.isPlaying){
+			tempSource.Play();
+		}
+		Destroy(temp, clip.length);
 	}
 }
