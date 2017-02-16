f21addbe9a88c76700eb4d32af8c9d9153f3ea02
Joe-PC\Joe <joe.gasparich@hotmail.com>
2017-02-16 18:28:18 +1300

    Music fades out before changing

--- Game/ReptileRampage/Assets/Scripts/MusicPlayer.cs
+++ Game/ReptileRampage/Assets/Scripts/MusicPlayer.cs
@@ -13,6 +13,9 @@ public class MusicPlayer : MonoBehaviour {
 	private Player playerScript;
 	public GameObject player;
 	public static bool won;
+	public static bool fadeToBoss = false;
+	public static bool fadeToLevel = false;
+
 	void Start () {
 		musicPlayer = GetComponent<AudioSource>();
 		hudManager = canvas.GetComponent<HUDManager>();
@@ -20,29 +23,49 @@ public class MusicPlayer : MonoBehaviour {
 		if(player != null){	
 			playerScript = player.GetComponent<Player>();
 		}
-		if(titleScreen){
+		if (titleScreen) {
 			musicPlayer.clip = levelMusic;
-			musicPlayer.Play();
+			musicPlayer.Play ();
+		} else {
+			bossMusicPlayed = false;
+			levelMusicPlayed = true;
+			musicPlayer.volume = 0.5f;
 		}
 	}
 	
 	void Update () {
+		if (fadeToBoss) {
+			if (musicPlayer.volume > 0.05) { 
+				musicPlayer.volume *= 0.95f;
+			} else {
+				musicPlayer.clip = bossMusic;
+				musicPlayer.Play();
+				bossMusicPlayed = true;
+				levelMusicPlayed = false;
+				fadeToBoss = false;
+				musicPlayer.volume = 0.5f;
+			}
+		}
+		if (fadeToLevel) {
+			if (musicPlayer.volume > 0.05) { 
+				musicPlayer.volume *= 0.95f;
+			} else {
+				musicPlayer.clip = levelMusic;
+				musicPlayer.Play();
+				bossMusicPlayed = false;
+				levelMusicPlayed = true;
+				fadeToLevel = false;
+				musicPlayer.volume = 0.5f;
+			}
+		}
 		if(!titleScreen){
 			if(playerScript.gameOver || won){
 				musicPlayer.loop = false;
 			}
-			if(hudManager.inBossFight && !bossMusicPlayed){
-				musicPlayer.clip = bossMusic;
-				musicPlayer.volume = 0.5f;
-				musicPlayer.Play();
-				bossMusicPlayed = true;
-				levelMusicPlayed = false;
-			}else if(!hudManager.inBossFight && !levelMusicPlayed){
-				musicPlayer.clip = levelMusic;
-				musicPlayer.volume = 0.2f;
-				musicPlayer.Play();
-				levelMusicPlayed = true;
-				bossMusicPlayed = false;
+			if(hudManager.inBossFight && !bossMusicPlayed && !fadeToBoss){
+				fadeToBoss = true;
+			}else if(!hudManager.inBossFight && !levelMusicPlayed && !fadeToLevel){
+				fadeToLevel = true;
 			}
 		}
 	}
